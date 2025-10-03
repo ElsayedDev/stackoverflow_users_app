f612f71b13a7cc24d3e1e84bac252aba2793501a
Ahmed Elsayed <elsayeddev@gmail.com>
2025-10-03 20:51:15 +0300

    feat(assets): add generated asset classes for colors and icons

---
+++ lib/gen/colors.gen.dart
@@ -0,0 +1,55 @@
+// dart format width=100
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: deprecated_member_use,directives_ordering,implicit_dynamic_list_literal,unnecessary_import
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class ColorName {
+  ColorName._();
+
+  /// Color: #CC9966
+  static const Color sofBadgeBronze = Color(0xFFCC9966);
+
+  /// Color: #FFCC01
+  static const Color sofBadgeGold = Color(0xFFFFCC01);
+
+  /// Color: #B4B8BC
+  static const Color sofBadgeSilver = Color(0xFFB4B8BC);
+
+  /// Color: #F8F9F9
+  static const Color sofBg = Color(0xFFF8F9F9);
+
+  /// Color: #0077CC
+  static const Color sofBlue = Color(0xFF0077CC);
+
+  /// Color: #D6D9DC
+  static const Color sofBorder = Color(0xFFD6D9DC);
+
+  /// Color: #C92A2A
+  static const Color sofDanger = Color(0xFFC92A2A);
+
+  /// Color: #3B4045
+  static const Color sofMuted = Color(0xFF3B4045);
+
+  /// Color: #F48024
+  static const Color sofOrange = Color(0xFFF48024);
+
+  /// Color: #127A2A
+  static const Color sofSuccess = Color(0xFF127A2A);
+
+  /// Color: #E1ECF4
+  static const Color sofTagBg = Color(0xFFE1ECF4);
+
+  /// Color: #39739D
+  static const Color sofTagText = Color(0xFF39739D);
+
+  /// Color: #0C0D0E
+  static const Color sofText = Color(0xFF0C0D0E);
+}
